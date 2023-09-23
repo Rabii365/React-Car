43f557315fcdaa5f89afa316d0530a7753ed73a1
Rabii365 <rabiithebig@gmail.com>
2023-09-23 03:44:12 +0300

    Updated Car Images API

--- utils/index.ts
+++ utils/index.ts
@@ -39,7 +39,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   const { make, year, model } = car;
 
-  url.searchParams.append("customer", "firabibomselekcompany");
+  url.searchParams.append("customer", "hrjavascript-mastery");
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
